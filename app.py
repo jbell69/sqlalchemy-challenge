29fc4738b11d6723c503bb171b841282993b89ce
Jason Bell <jbell69@mac.com>
2019-11-07 12:44:56 -0600

    version 3

--- app.py
+++ app.py
@@ -1,1 +1,158 @@
-	
+# import dependances
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# import Flask, jsonify
+from flask import Flask, jsonify
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create an app
+app = Flask(__name__)
+
+
+# Index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (
+        f"Welcome to Hawaii Climate homepage!<br/>"
+	    f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+	    f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_end_date/<end>"
+    )
+
+# Precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of precipition data by date"""
+    # Query all measurement
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of precipitations
+    all_precipitations = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["Precipitation"] = prcp
+        
+        all_precipitations.append(precipitation_dict)
+
+    return jsonify(all_precipitations)
+
+# Stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    print("Server received request for 'Stations' page...")
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all Stations"""
+    # Query all stations
+    results = session.query(Station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    stations = list(np.ravel(results))
+
+    return jsonify(stations) 
+
+# Observed Temperature route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("Server received request for 'Observed Temperature' page...")
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all Stations"""
+    # Query for all temperature for last year
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    Temp_last_year = session.query(Measurement.date,Measurement.tobs).\
+    filter((Measurement.date) >= query_date).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    temps = list(np.ravel(Temp_last_year))
+
+    return jsonify(temps)
+
+# Start Date route
+@app.route("/api/v1.0/start_date")
+def start_date():
+    print("Server received request for 'Start Date Temperature' page...")
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query for max, min, avg temperature for starting date
+
+    query_date = dt.date(2011, 8, 23)
+
+    start_date_temps = session.query(
+        (func.min(Measurement.tobs)),
+        (func.max(Measurement.tobs)),
+        (func.avg(Measurement.tobs))).\
+    filter(Measurement.date >= query_date).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    temps = list(np.ravel(start_date_temps))
+
+    return jsonify(temps)
+
+# Start and End Date route
+@app.route("/api/v1.0/start_end_date")
+def start_end_date():
+    print("Server received request for 'Start/End Date Temperature' page...")
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query for max, min, avg temperature for starting date
+
+    query_date = dt.date(2011, 8, 23)
+
+    start_end_date_temps = session.query(
+        (func.min(Measurement.tobs)),
+        (func.max(Measurement.tobs)),
+        (func.avg(Measurement.tobs))).\
+    filter(Measurement.date >= query_date).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    temps = list(np.ravel(start_end_date_temps))
+
+    return jsonify(temps)
+
+if __name__ == "__main__":
+    app.run(debug=True)	
